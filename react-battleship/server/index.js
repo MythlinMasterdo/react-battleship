cb772b32880d5eeba29ca2a464be508f3a76996e
Miles Smith <miles71397@gmail.com>
2017-01-04 11:48:02 -0800

    setup basic server and react components

---
+++ react-battleship/server/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var parser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+  res.send('hello world!');
+})
+
+app.listen(port, function() {
+  console.log('listening on port ', port);
+});
+
+module.exports = app;
